419f243e2221e0d7b5cfcbdcc3e695f0bd52037b
Wouter Deconinck <wdconinc@gmail.com>
2020-10-21 14:08:39 -0500

    Create setup-cvmfs.sh

---
+++ setup-cvmfs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install cvmfs
+wget -q https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
+sudo dpkg -i cvmfs-release-latest_all.deb
+sudo apt-get -q update
+sudo apt-get -q -y install cvmfs cvmfs-config-default
+rm -f cvmfs-release-latest_all.deb
+
+# Setup eic.opensciencegrid.org
+sudo mkdir -p /etc/cvmfs
+echo "CVMFS_REPOSITORIES=${INPUT_CVMFS_REPOSITORIES:-atlas.cern.ch,atlas-condb.cern.ch,grid.cern.ch}" | sudo tee /etc/cvmfs/default.local
+echo "CVMFS_HTTP_PROXY=${INPUT_CVMFS_HTTP_PROXY:-DIRECT}" | sudo tee -a /etc/cvmfs/default.local
+sudo cvmfs_config setup
+
+# Configure autofs
+echo "+dir:/etc/auto.master.d" | sudo tee /etc/auto.master
+sudo mkdir -p /etc/auto.master.d
+echo "/cvmfs /etc/auto.cvmfs" | sudo tee /etc/auto.master.d/cvmfs.autofs
+sudo service autofs start
