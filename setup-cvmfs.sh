53c965cced3f2d5d9bee5d21ebb3024f6caadde2
Marko Petric <marko.petric@cern.ch>
2020-12-18 16:51:51 +0100

    Check return code cvmfs_config at the end of installation

--- setup-cvmfs.sh
+++ setup-cvmfs.sh
@@ -35,6 +35,12 @@ fi
 
 echo "Run cvmfs_config setup"
 sudo cvmfs_config setup
+retCongif=$?
+if [ $retCongif -ne 0 ]; then
+  echo "!!! github-action-cvmfs FAILED !!!"
+  echo "cvmfs_config setup exited with ${retCongif}"
+  exit $retCongif
+fi
 
 
 if [ "$(uname)" == "Darwin" ]; then
